d7c336c6ebcb6ac0349bff4e86fdd9146d958709
margaret254 <maggiemwas91@gmail.com>
2020-01-08 15:13:28 +0300

    Add login urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -17,10 +17,15 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views 
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-     url(r'',include('insta.urls'))
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'',include('insta.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+    url(r'^tinymce/', include('tinymce.urls')),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
